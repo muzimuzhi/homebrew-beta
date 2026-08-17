88bde363069858f1837b420f2a3fb85baeddf7c2
Yukai Chou <muzimuzhi@gmail.com>
2026-08-18 03:06:01 +0800

    fix style

--- Casks/texstudio@beta.rb
+++ Casks/texstudio@beta.rb
@@ -41,16 +41,16 @@ cask "texstudio@beta" do
   # https://docs.brew.sh/Cask-Cookbook#target-should-only-be-used-in-select-cases
   app "texstudio-#{version}-osx#{arch}.app"
 
-  # Check Bundle ID with `brew list-running-app-ids`. Locally cloned cask tap needed, run
-  # `brew tap --force homebrew/cask`.
-  # https://docs.brew.sh/Cask-Cookbook#uninstall-quit
-  uninstall quit: "texstudio"
-
   # learnt from https://github.com/Homebrew/homebrew-cask/blob/03a0edb4616198f6f64b285dbf842bc3b73a7f31/Casks/p/parallels.rb#L36-L41
   postflight do
     system_command "xattr",
                    args: ["-dr", "com.apple.quarantine", "#{appdir}/texstudio-#{version}-osx#{arch}.app"]
   end
+
+  # Check Bundle ID with `brew list-running-app-ids`. Locally cloned cask tap needed, run
+  # `brew tap --force homebrew/cask`.
+  # https://docs.brew.sh/Cask-Cookbook#uninstall-quit
+  uninstall quit: "texstudio"
 
   zap trash: [
     "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/texstudio.sfl*",
